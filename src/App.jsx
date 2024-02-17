7e698a49b76682d415d7ab7e9128728e5adc8f9a
IndexDDuo <83288588+IndexDDuo@users.noreply.github.com>
2024-02-17 05:19:34 +0000

    Feb 17, 2024, 12:19 AM

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import { Link, Element } from 'react-scroll'; //import link and element to scroll to different sections of the page
 
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; //import browser router to navigate between pages
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; //import browser router to navigate between pages
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Projects from './components/Projects/Projects';
 import Contact from './components/Contact/Contact';
+import NotFoundPage from './components/errorPage/NotFoundPage';
 
 function App() {
   return (
+    
     <div>
       <nav>
         <ul>
@@ -24,6 +26,16 @@ function App() {
       <Element name="about"><About /></Element>
       <Element name="projects"><Projects /></Element>
       <Element name="contact"><Contact /></Element>
+      <Router>
+            <Routes>
+              
+                {/* Define existing routes here */}
+                <Route path="/" element={<Home />} />
+                
+                {/* Catch-all route for undefined paths */}
+                <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+        </Router>
     </div>
   );
 }
