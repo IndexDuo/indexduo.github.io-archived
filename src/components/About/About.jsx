0a03788f444f2b66d875a9c2c02ffebce4a3d399
IndexDDuo <83288588+IndexDDuo@users.noreply.github.com>
2024-02-17 05:13:48 +0000

    Feb 17, 2024, 12:13 AM

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,7 +4,7 @@ function About() {
   return (
     <div>
       <h1>About Me</h1>
-      <p>I believe in...</p>
+      <p>I specialize in crafting web experiences that balance aesthetic appeal with accessibility, usability, and user experience. As someone who values empathy and consideration, my goal is to build websites that minimize user frustration and enhance engagement. My approach is rooted in a deep understanding of the needs of both businesses and their audiences, ensuring that every project I undertake not only meets but exceeds expectations, making every interaction with the digital world as seamless and impactful as possible.</p>
     </div>
   );
 }
