8ecb131cb6a5fa44e4eb3ec39fcdf2a4aa51448d
IndexDDuo <83288588+IndexDDuo@users.noreply.github.com>
2024-02-16 23:11:27 -0500

    Add global styles and new components

---
+++ src/components/About/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <p>This is the about page of my portfolio where I talk about myself.</p>
+    </div>
+  );
+}
+
+export default About;
