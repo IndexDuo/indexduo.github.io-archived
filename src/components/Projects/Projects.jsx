1135eb321d73d50bf2e23d3c13948fd70beffbee
IndexDDuo <83288588+IndexDDuo@users.noreply.github.com>
2024-02-17 05:32:42 +0000

    Feb 17, 2024, 12:32 AM

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -6,9 +6,19 @@ function Projects() {
     <div>
       <h1>What I do</h1>
       <p>This is the projects page.</p>
+      <Collapsible title="Web Design">
+        <p>Details about project 1</p>
+      </Collapsible>
+      <Collapsible title="UI/UX Design">
+        <p>Details about project 1</p>
+      </Collapsible>
+      <Collapsible title="Accessibility">
+        <p>Details about project 1</p>
+      </Collapsible>
       <Collapsible title="Project 1">
         <p>Details about project 1</p>
       </Collapsible>
+      
       
     </div>
   );
